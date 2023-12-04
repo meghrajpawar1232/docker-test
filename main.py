4db8fffa92d6ff98dae8d8f7f76576feb21c895e
meghrajpawar1232 <meghrajpawar2111@gmail.com>
2023-12-04 13:56:01 +0530

    main.py

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
-from flask import Flask
+# from flask import Flask
+
+# app = Flask(__name__)
+
+# @app.route('/')
+# def hello_world():
+#     return 'Hello, Thanks for visit Meghraj pawar!'
+
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=5000)
+
+from flask import Flask, send_from_directory
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, Thanks for visit Meghraj pawar!'
+    return 'Hello, World!'
+
+@app.route('/images/<filename>')
+def serve_image(filename):
+    return send_from_directory('static', filename)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+
